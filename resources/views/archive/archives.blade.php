948ca14e8a44860b496539a67c2d063fa892d7d4
Micko Widi <widimicko85@gmail.com>
2022-04-15 14:50:18 +0700

    add draw.io file

--- resources/views/archive/archives.blade.php
+++ resources/views/archive/archives.blade.php
@@ -6,7 +6,7 @@
 
 @section('main')
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Selamat Datang, {{ Auth::user()->name }} ,  {{ Auth::user()->role }}</h1>
+    <h1 class="h2">Selamat Datang, {{ Auth::user()->role !== 'admin' ? 'Admin '. Auth::user()->role : 'Administrator' }}</h1>
   </div>
 
   @if ($notification = Session::get('success'))
